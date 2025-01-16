9f85f7ca687432863726800cf247586066cf155a
Alan Thompson <alan@Mac.san.rr.com>
2025-01-16 11:05:38 -0800

    misc

--- src/demo/util.clj
+++ src/demo/util.clj
@@ -10,23 +10,7 @@
     [java.time LocalDate]
     ))
 
-; modified from clojure.core/with-out-str
-;(defmacro discarding-out-str
-;  [& body]
-;  "Evaluates exprs in a context in which *out* is bound to a fresh StringWriter.
-;  Discards the string created by any nested printing calls."
-;  `(let [s# (new java.io.StringWriter)]
-;     (binding [*out* s#]
-;       ~@body)))
-
-;-----------------------------------------------------------------------------
-(s/defn iowa-prefix? :- s/Bool
-  [s :- s/Str]
-  (t/with-exception-default false ; in case less than 3 chars
-    (t/truthy? (= "ia-" (subs s 0 3)))))
-
-;-----------------------------------------------------------------------------
-(s/defn fn->vec-fn :- tsk/Fn
+(s/defn fn-blockify :- tsk/Fn
   "Vectorize a function, so that instead of operating on a scalar value,
   it operates on each value in a 1D array. Used twice, the resulting function operates
   on each value in a 2D array. Not lazy."
@@ -34,7 +18,7 @@
   (s/fn [block :- [s/Any]]
     (mapv f block)))
 
-(s/defn fn->vec-fn-lazy :- tsk/Fn
+(s/defn fn-blockify-lazy :- tsk/Fn
   "Vectorize a function, so that instead of operating on a scalar value,
   it operates on each value in a 1D array. Used twice, the resulting function operates
   on each value in a 2D array. Lazy."
@@ -42,24 +26,24 @@
   (s/fn [block :- [s/Any]]
     (map f block)))
 
-(s/defn array-1d->2d-lazy :- [[s/Any]]
+(s/defn data-blockify-lazy :- [[s/Any]]
   "Convert a 1D sequence to a lazy 2D array (possibly ragged) in row-major order."
-  [row-size :- s/Int
-   seq-1d :- [s/Any]]
-  (partition-all row-size seq-1d))
+  [N :- s/Int
+   data :- [s/Any]]
+  (partition-all N data))
 
-(s/defn array-1d->2d :- [[s/Any]]
-  "Convert a 1D sequence to a 2D array (possibly ragged) in row-major order."
-  [row-size :- s/Int
-   seq-1d :- [s/Any]]
-  (unlazy (array-1d->2d-lazy row-size seq-1d)))
+(s/defn data-blockify :- [[s/Any]]
+  "Convert a 1D sequence to a 2D array (possibly ragged) in row-major order. Not lazy."
+  [N :- s/Int
+   data :- [s/Any]]
+  (unlazy (data-blockify-lazy N data)))
 
-(s/defn array-2d->1d :- [s/Any]
+(s/defn data-ublockify :- [s/Any]
   "Concatenate rows of a 2D array (possibly ragged), returning a 1-D vector."
-  [seq-2d :- [[s/Any]]]
-  (apply glue seq-2d))
+  [blocks :- [[s/Any]]]
+  (apply glue blocks))
 
-(s/defn array-2d->1d-lazy :- [s/Any]
+(s/defn data-unblockify-lazy :- [s/Any]
   "Concatenate rows of a 2D array (possibly ragged), returning a 1-D vector."
   [seq-2d :- [[s/Any]]]
   (apply concat seq-2d))
@@ -74,33 +58,3 @@
         (doseq [item row]
           (yield item))))))
 
-#_(s/defn array-2d->1d-lazy-recur :- [s/Any]
-    "Concatenate rows of a 2D array (possibly ragged), returning a 1-D vector."
-    [seq-2d :- [[s/Any]]]
-    (loop [rows seq-2d]
-      (let [row-curr  (first rows)
-            rows-next (rest rows)]
-
-        )
-      )
-
-    (lazy-gen
-      (doseq [row seq-2d]
-        (doseq [item row]
-          (yield item)))))
-
-;-----------------------------------------------------------------------------
-(s/defn date-str-mmddyyyy->iso :- s/Str
-  "Convert a date string like `07142024` to ISO format like `2024-07-14`"
-  [date-str :- s/Str]
-  (let [date-str (str/trim date-str)
-        nchars   (count date-str)]
-    (assert-info (= 8 nchars) "Invalid length" (vals->map date-str nchars))
-    (assert-info (re-matches #"\p{Digit}+" date-str)
-      "date string must be all digits" (vals->map date-str))
-    (let [mm     (subs date-str 0 2)
-          dd     (subs date-str 2 4)
-          yyyy   (subs date-str 4 8)
-          result (str yyyy "-" mm "-" dd)]
-      (LocalDate/parse result) ; parse result to validate legal values
-      result)))
--- test/tst/demo/util.clj
+++ test/tst/demo/util.clj
@@ -1,5 +1,7 @@
 (ns tst.demo.util
-  (:use demo.util tupelo.core tupelo.test)
+  (:use demo.util
+        tupelo.core
+        tupelo.test)
   (:require
     [tupelo.profile :as prof]
     [schema.core :as s]
@@ -8,42 +10,23 @@
 (def verbose? true)
 
 (verify
-  (let [s "abcdef"]
-    (is= "abc" (subs s 0 3)))
-  (is (iowa-prefix? "ia-plan"))
-  (is (iowa-prefix? "ia-"))
-  (is (iowa-prefix? "ia-medicare"))
-  (isnt (iowa-prefix? "ia"))
-  (isnt (iowa-prefix? "id-"))
-  (isnt (iowa-prefix? "id-medicare"))
-  )
+  (let [seq1            (range 5)
+        arr1            (data-blockify 2 seq1)
+        seq2            (data-ublockify arr1)
 
-(verify
-  (is= "2024-07-14" (date-str-mmddyyyy->iso "07142024"))
-  (throws? (date-str-mmddyyyy->iso "7142024"))
-  (throws? (date-str-mmddyyyy->iso "99887766")) ; validaion detects out-of-range values
-  (throws? (date-str-mmddyyyy->iso "xxyy7766")) ; invalid chars )
-  )
+        inc-1d          (fn-blockify inc)
+        inc-2d          (fn-blockify inc-1d)
 
-;-----------------------------------------------------------------------------
-(verify
-  (let [seq1          (range 5)
-        arr1          (array-1d->2d 2 seq1)
-        seq2          (array-2d->1d arr1)
+        inc-1d-lazy     (fn-blockify-lazy inc)
+        inc-2d-lazy     (fn-blockify-lazy inc-1d-lazy)
 
-        inc-1d        (fn->vec-fn inc)
-        inc-2d        (fn->vec-fn inc-1d)
+        seq1-inc-a      (inc-1d seq1)
+        arr1-inc        (inc-2d arr1)
+        seq1-inc-b      (data-ublockify arr1-inc)
 
-        inc-1d-lazy        (fn->vec-fn-lazy inc)
-        inc-2d-lazy        (fn->vec-fn-lazy inc-1d-lazy)
-
-        seq1-inc-a    (inc-1d seq1)
-        arr1-inc      (inc-2d arr1)
-        seq1-inc-b    (array-2d->1d arr1-inc)
-
-        seq1-inc-a-lazy    (inc-1d-lazy seq1)
-        arr1-inc-lazy      (inc-2d-lazy arr1)
-        seq1-inc-b-lazy    (array-2d->1d-lazy arr1-inc)
+        seq1-inc-a-lazy (inc-1d-lazy seq1)
+        arr1-inc-lazy   (inc-2d-lazy arr1)
+        seq1-inc-b-lazy (data-unblockify-lazy arr1-inc)
         ]
     (is= seq1 [0 1 2 3 4])
     (is= arr1 [[0 1]
@@ -59,12 +42,59 @@
 
     (is= seq1-inc-a-lazy [1 2 3 4 5])
     (is= arr1-inc-lazy [[1 2]
-                   [3 4]
-                   [5]])
+                        [3 4]
+                        [5]])
     (is= seq1-inc-b-lazy [1 2 3 4 5])
-    ))
+    )
 
-(verify
+  (when false
+    (let-spy
+      [d1 (thru 0 4)
+       d2 (thru 1 5)
+
+       s1 (mapv + d1 d2)
+       I1 (mapv inc d1)
+       I2 (mapv inc d2)
+       ]
+      (is= d1 [0 1 2 3 4])
+      (is= d2 [1 2 3 4 5])
+      (is= s1 [1 3 5 7 9])
+
+      (is= I1 [1 2 3 4 5])
+      (is= I2 [2 3 4 5 6]))))
+
+(verify-focus
+  (let-spy
+    [vecsum     (fn [v] (apply + v))
+     vecsum-blk (fn-blockify vecsum)
+
+     v1         (thru 1 10)
+     v1s        (vecsum v1)
+
+     b1         (data-blockify 3 v1)
+     b1s1       (forv [b b1]
+                  (vecsum b))
+
+     b1s2       (vecsum-blk b1)
+     b1f1        (vecsum b1s1)
+     b1f2        (vecsum b1s2)
+     ]
+    (is= v1s 55)
+
+    (is= b1 [[1 2 3]
+             [4 5 6]
+             [7 8 9]
+             [10]])
+    (is= b1s1 [6 15 24 10])
+    (is= b1s2 [6 15 24 10])
+    (is= b1f1 55)
+    (is= b1f2 55)
+
+    )
+  )
+
+
+(verify-focus
   ; ***** Need to disable Plumatic Schema validation of arguments or destroys laziness!!! *****
   (s/without-fn-validation
 
@@ -73,20 +103,23 @@
     ; 2024-11-18 will overflow for N=39999
     (let [N                     9999 ; Note:  Use 9999 for real stress test
           num-triangle-elements (it-> N
-                                  (* it (inc it))
-                                  (/ it 2))
+                                      (* it (inc it))
+                                      (/ it 2))
           first-37              [1 1 2 1 2 3 1 2 3 4 1 2 3 4 5 1 2 3 4 5 6 1 2 3 4 5 6 7 1 2 3 4 5 6 7 8 1]
 
           lazy-fn               (fn lazy-fn []
                                   (let [triangle-2d (map #(range 1 (inc %)) (range 1 (inc N)))
-                                        triangle-1d (array-2d->1d-lazy triangle-2d)]
-                                    (is= first-37 (take 37 triangle-1d))))
+                                        triangle-1d (data-unblockify-lazy triangle-2d)]
+                                    (is= first-37 (take 37 triangle-1d))
+                                    (is= num-triangle-elements (count triangle-1d))
+                                    ))
 
           eager-fn              (fn eager-fn []
                                   (let [triangle-2d (mapv #(thru 1 %) (thru 1 N))
-                                        triangle-1d (array-2d->1d triangle-2d)]
+                                        triangle-1d (data-ublockify triangle-2d)]
                                     (is= first-37 (take 37 triangle-1d))
-                                    (is= num-triangle-elements (count triangle-1d))))]
+                                    (is= num-triangle-elements (count triangle-1d))
+                                    ))]
 
       ; Results:  N=9999  (2024-11-18 Mac Studio)
       ;   :with-timer-print :lazy   0.000087
@@ -106,24 +139,24 @@
   ; ***** Need to disable Plumatic Schema validation of arguments or destroys laziness!!! *****
   (s/without-fn-validation
 
-    (let [N        1e7 ; Note:  Use 9999 for real stress test
+    (let [N        1e7
           ncols    (long (Math/round (Math/sqrt N)))
           vals     (range N)
 
           lazy-fn  (fn lazy-fn []
-                     (let [data-2d (array-1d->2d-lazy ncols vals)
-                           data-1d (array-2d->1d-lazy data-2d)]
+                     (let [data-2d (data-blockify-lazy ncols vals)
+                           data-1d (data-unblockify-lazy data-2d)]
                        (is= (range 37) (take 37 data-1d))))
 
           #_(comment
-            lazy-fn-gen (fn lazy-fn []
-                          (let [data-2d (array-1d->2d-lazy ncols vals)
-                                data-1d (array-2d->1d-lazy-gen data-2d)]
-                            (is= (range 37) (take 37 data-1d)))))
+              lazy-fn-gen (fn lazy-fn []
+                            (let [data-2d (data-blockify-lazy ncols vals)
+                                  data-1d (array-2d->1d-lazy-gen data-2d)]
+                              (is= (range 37) (take 37 data-1d)))))
 
           eager-fn (fn eager-fn []
-                     (let [data-2d (array-1d->2d ncols vals)
-                           data-1d (array-2d->1d data-2d)]
+                     (let [data-2d (data-blockify ncols vals)
+                           data-1d (data-ublockify data-2d)]
                        (is= (range 37) (take 37 data-1d))))
           ]
 
