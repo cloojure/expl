e919ca25584cd472a28bea8f825bfdafff13f881
Alan Thompson <cloojure@gmail.com>
2025-02-02 17:46:08 -0800

    misc

--- test/tst/demo/blk.clj
+++ test/tst/demo/blk.clj
@@ -13,43 +13,46 @@
    bs :- [s/Num]]
   (mapv + as bs))
 
+; how to wrap a function so it prints execution time
+(s/defn vadd2-time
+  [& args]
+  (prof/with-timer-print :vadd2-time
+    (apply vadd2 args)))
+
 (s/defn vadd2-blk-expl :- [s/Num]
   "An explicit block version of vadd2"
   [N :- s/Int
    as :- [s/Num]
    bs :- [s/Num]]
-  (let [ablks  (partition-all N as)
-        bblks  (partition-all N bs)
-        rblks  (mapv vadd2 ablks bblks)
-        result (apply glue rblks)]
-    result))
+  (prof/with-timer-print :vadd2-blk-expl
+    (let [ablks  (partition-all N as)
+          bblks  (partition-all N bs)
+          rblks  (mapv vadd2 ablks bblks)
+          result (apply glue rblks)]
+      result)))
 
 (verify
-  (let [as         (thru 0 9)
-        bs         (thru 1 10)
+  (let [data-1    (thru 0 9)
+        data-2    (thru 1 10)
 
-        r1         (vadd2 as bs)
-        r2         (vadd2-blk-expl 7 as bs)
-
-        ; how to wrap a function so it prints execution time
-        vadd2-time (fn [& args]
-                     (prof/with-timer-print :vadd2-time
-                       (apply vadd2 args)))
+        r1        (vadd2-time data-1 data-2)
+        r2        (vadd2-blk-expl 3 data-1 data-2)
+        >>        (newline)
 
         ; convert any function into a block function
-        vadd2-blk  (block-fn 3 vadd2-time
-                             {:enter (fn [ctx] (prn :enter (:index ctx)))})
-        rs-blk     (vadd2-blk as bs)
-        r-blk      (apply glue rs-blk)
+        vadd2-blk (block-fn 3 vadd2-time
+                            {:enter (fn [ctx] (prn :enter (:index ctx)))})
+        r3-blk    (vadd2-blk data-1 data-2)
+        r3        (apply glue r3-blk)
         ]
-    (is= as [0 1 2 3 4 5 6 7 8 9])
-    (is= bs [1 2 3 4 5 6 7 8 9 10])
+    (is= data-1 [0 1 2 3 4 5 6 7 8 9])
+    (is= data-2 [1 2 3 4 5 6 7 8 9 10])
 
-    (is= rs-blk [[1 3 5]
+    (is= r3-blk [[1 3 5]
                  [7 9 11]
                  [13 15 17]
                  [19]])
     (is= [1 3 5 7 9 11 13 15 17 19]
-      r1 r2 r-blk)
+      r1 r2 r3)
     ))
 
